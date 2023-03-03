bc8fc1b24636ae07ce0516b31ca0c9f98d6880d9
sbojorge <sara.bojorge@gmail.com>
2023-03-03 18:21:44 +0000

    Run 3 tests and write the corresponding code for making them pass

--- test_student.py
+++ test_student.py
@@ -3,7 +3,19 @@ from student import Student
 
 
 class TestStudent(unittest.TestCase):
-    pass
+    def test_full_name(self):
+        student = Student("John", "Doe")
+        self.assertEqual(student.full_name, "John Doe")
+
+    def test_alert_santa(self):
+        student = Student("John", "Doe")
+        student.alert_santa()
+
+        self.assertTrue(student.naughty_list)
+    
+    def test_email(self):
+        student = Student("John", "Doe")
+        self.assertEqual(student.email, "john.doe@email.com")
 
 
 if __name__ == '__main__':

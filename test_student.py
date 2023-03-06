fa7296f498902896b38a0f23d9840960d6b92488
sbojorge <sara.bojorge@gmail.com>
2023-03-06 09:40:54 +0000

    Add the setUp and tearDown methods for shortening code and demonstration purposes

--- test_student.py
+++ test_student.py
@@ -3,19 +3,25 @@ from student import Student
 
 
 class TestStudent(unittest.TestCase):
+    def setUp(self):
+        print('setUp')
+        self.student = Student("John", "Doe")
+
+    def tearDown(self):
+        print('tearDown')
+
     def test_full_name(self):
-        student = Student("John", "Doe")
-        self.assertEqual(student.full_name, "John Doe")
+        print('test_full_name')
+        self.assertEqual(self.student.full_name, "John Doe")
 
     def test_alert_santa(self):
-        student = Student("John", "Doe")
-        student.alert_santa()
+        print('test_alert_santa')
+        self.student.alert_santa()
+        self.assertTrue(self.student.naughty_list)
 
-        self.assertTrue(student.naughty_list)
-    
     def test_email(self):
-        student = Student("John", "Doe")
-        self.assertEqual(student.email, "john.doe@email.com")
+        print('test_email')
+        self.assertEqual(self.student.email, "john.doe@email.com")
 
 
 if __name__ == '__main__':
